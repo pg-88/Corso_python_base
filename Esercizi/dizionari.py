58f1f88487a3612452897f5915181889beee1714
pg-88 <grigolopi@gmail.com>
2024-05-21 16:48:57 +0200

    errori corretti

--- Esercizi/dizionari.py
+++ Esercizi/dizionari.py
@@ -36,3 +36,22 @@ nomi = [
 
 nazioni = ["slo", "gb", "pt", "it", "fr", "nl", "ir", "no", "de", "be"]
 
+
+"""Creare un dizionario che abbia come chiave il nome dell'atleta e come valore un dizionario che contenga:
+Posizione: numero in classifica,
+nazionalità: sigla nazione"""
+
+
+"""Da questo testo esempio, contare quante parole ci sono e con quanta frequenza si ripetono all'interno del testo.
+Utilizzare un dizionario le cui chiavi saranno le parole e i valori saranno le frequenze."""
+
+testo_esempio = """Mi chiamo Sofia. Ho 35 anni. Mio marito si chiama Alessandro e ha 36 anni. La mia famiglia è composta in tutto da cinque persone. Io e mio marito abbiamo tre figli.
+
+Viviamo in un piccolo paese di campagna, in una bella casa con un cortile. I nostri tre figli si chiamano Andrea, Martina e Giacomo. Andrea frequenta l'asilo, Martina e Giacomo frequentano le scuole elementari. I miei tre figli amano giocare a tanti giochi diversi nel cortile.
+
+I genitori di mio marito vivono lontano da qui, in città. I miei genitori invece abitano vicino a noi, nello stesso paese. Vogliono molto bene ai nostri tre figli e spesso si occupano di loro.
+
+Vicino a noi abita mia sorella Giulia con la sua famiglia. I miei figli giocano spesso con Marta, la figlia di Giulia.
+
+Io sono una casalinga che lavora inoltre a casa come articolista. Mio marito è invece un operaio. È una persona molto buona e gioca spesso con i suoi tre figli. Sono molto felice della mia famiglia! """
+
